6d76620399b8d83b9f792a34fb89b57586f2d59b
promethean2020 <75622659+promethean2020@users.noreply.github.com>
2025-06-22 02:23:00 +0500

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -37,7 +37,7 @@ const Header = () => {
     { href: '/', label: 'Home' },
     { href: '/services', label: 'Services' },
     { href: '/about', label: 'About' },
-    { href: '/contact-us', label: 'Contact' }
+    { href: '/contact', label: 'Contact' }
   ];
 
   return (
